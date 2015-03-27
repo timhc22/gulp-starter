232eb55a433fd540673fd2a102b26fd7e4a1c089
Timothy Coleman <timhc22@gmail.com>
2015-03-27 18:15:18 +0000

    up to tutorial2 on here:http://arturadib.com/hello-backbonejs/docs/2.html

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -4,19 +4,49 @@
 // `require` array. Also in config.js, jquery is listed in `external` array for this bundle. This combination lets this
 // file use the jquery module bundled with global.js, instead of including it twice!
 
-var $ = require('jquery');
 var _ = require('underscore');
+var Backbone = require('backbone');
+var $ = require('jquery');
+Backbone.$ = $;
 
 $(document)
     .ajaxStart(function () {})
     .ajaxStop(function () {})
 ;
 
-$(document).ready(function()
+$(function()
 {
     $(".hiButton").on( "click", function() {
       alert('hi');
     });
 
-});
 
+    var ListView = Backbone.View.extend({
+        el: $('.backbone'), // attaches `this.el` to an existing element.
+
+        events: {
+            'click button#add': 'addItem'
+        },
+        initialize: function(){
+            // every function that uses 'this' as the current object should be in here
+            _.bindAll(this, 'render', 'addItem'); // fixes loss of context for 'this' within methods
+
+            this.counter = 0; //total nubmer of items added so far
+            this.render(); // not all views are self-rendering. This one is.
+        },
+
+        render: function(){
+            $(this.el).append("<button id='add'>Add list item</button>");
+            $(this.el).append("<ul></ul>");
+        },
+
+        addItem: function(){
+            this.counter++;
+            $('ul', this.el).append("<li>hello world"+this.counter+"</li>");
+        }
+
+    });
+
+    var listView = new ListView();
+
+});
