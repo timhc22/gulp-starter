063a4e642a6062aa68400602004b027d069b8273
Timothy Coleman <timhc22@gmail.com>
2015-03-28 00:55:34 +0000

    end of tutorial 3

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -21,29 +21,61 @@ $(function()
     });
 
 
+    var Item = Backbone.Model.extend({
+        defaults: {
+            part1: 'hello',
+            part2: 'world'
+        }
+    });
+
+    var List = Backbone.Collection.extend({
+        model: Item
+    });
+
     var ListView = Backbone.View.extend({
         el: $('.backbone'), // attaches `this.el` to an existing element.
 
         events: {
             'click button#add': 'addItem'
         },
+
         initialize: function(){
             // every function that uses 'this' as the current object should be in here
-            _.bindAll(this, 'render', 'addItem'); // fixes loss of context for 'this' within methods
+            _.bindAll(this, 'render', 'addItem', 'appendItem'); // fixes loss of context for 'this' within methods
+
+            this.collection = new List();
+            this.collection.bind('add', this.appendItem); //collection event binder
 
             this.counter = 0; //total nubmer of items added so far
             this.render(); // not all views are self-rendering. This one is.
         },
 
         render: function(){
+
+            //Save reference to this so it can be accessed from within the scope of the callback below
+            var self = this;
             $(this.el).append("<button id='add'>Add list item</button>");
             $(this.el).append("<ul></ul>");
+            _(this.collection.models).each(function(item){ //in case collection is not empty
+                self.appendItem(item);
+            }, this);
         },
 
+        //addItem() now deals solely with models/collections.
         addItem: function(){
             this.counter++;
-            $('ul', this.el).append("<li>hello world"+this.counter+"</li>");
+            var item = new Item();
+            item.set({
+               part2: item.get('part2') + this.counter //modify item details
+            });
+            this.collection.add(item); // add item to collection; view is updated via event 'add'
+        },
+
+        //View updates are delegated to the add event listener appendItem() below.
+        appendItem: function(item){
+            $('ul', this.el).append("<li>"+item.get('part1')+" "+item.get('part2')+"</li>");
         }
+
 
     });
 
