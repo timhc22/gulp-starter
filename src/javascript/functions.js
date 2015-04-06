abc10a22a76cd945c4bd9a9ad9e8f6890f005cc4
Timothy Coleman <timhc22@gmail.com>
2015-04-06 20:52:04 +0100

    js enhancement for menu

--- src/javascript/functions.js
+++ src/javascript/functions.js
@@ -20,6 +20,27 @@ $(function()
       alert('hi');
     });
 
+
+
+
+
+    // Menu
+    $(document).click(function(event) {
+        //jquery enhancement for menu, close if click off the menu
+        if(!$(event.target).closest('.js-navigation').length) {
+            $(".js-nav-checkbox").prop('checked', false);
+        }
+    });
+// End Menu
+
+//$(".js-navigation").on( "click", function() {
+//    alert('hi');
+//});
+
+
+
+
+
     //Overrides persistence storage with dummy function. This enables use of Model.destroy() without raising an error.
     Backbone.sync = function(method, model, success, error){
         success();
