7b1abe9c00cd54024262f7cc932291e440637a53
Timothy Coleman <timhc22@gmail.com>
2015-06-12 16:24:50 +0100

    end of tutorial 10

--- src/javascript/_react.js
+++ src/javascript/_react.js
@@ -1,6 +1,60 @@
 var React = require('react');
+var Marked = require('marked');
+
+var data = [
+    {author: "Pete Hunt", text: "This is one comment"},
+    {author: "Jordan Walke", text: "This is yet *another* comment"}
+];
+
+
+var Comment = React.createClass({
+    render: function() {
+        var rawMarkup = Marked(this.props.children.toString(), {sanitize: true});
+        return (
+            React.createElement('div', {className: "comment"},
+                React.createElement('h2', {className: "commentAuthor"}, this.props.author),
+                React.createElement('span', {dangerouslySetInnerHTML: {__html: rawMarkup}})
+            )
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function (comment) {
+           return (
+               React.createElement(Comment, {author: comment.author, children: comment.text})
+           );
+        });
+        return (
+            React.createElement('div', {className: "commentList"}, commentNodes)
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            React.createElement('div', {className: "commentForm"},
+                "Hello, world! I am a CommentForm."
+            )
+        );
+    }
+});
+
+var CommentBox = React.createClass({displayName: 'CommentBox',
+    render: function() {
+        return (
+            React.createElement('div', {className: "commentBox"},
+                React.createElement('h1', null, 'Comments'),
+                React.createElement(CommentList, {data: this.props.data}),
+                React.createElement(CommentForm, null)
+            )
+        );
+    }
+});
 
 React.render(
-    React.createElement('h1', null, 'Hello, world!'),
-    document.getElementById('example')
+    React.createElement(CommentBox, {data: data}),
+    document.getElementById('content')
 );
